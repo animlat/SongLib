9719463846a6e985a679858381aad98a7730fff0
William Wong <wwongqwerty@gmail.com>
2019-10-01 15:48:59 -0400

    hello anim this is a test haha

---
+++ src/app/SongLib.java
@@ -0,0 +1,26 @@
+package app;
+
+//--module-path "C:\javafx-sdk-13\lib" --add-modules javafx.controls,javafx.fxml
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import view.SongLibController;
+
+public class SongLib extends Application {
+
+	@Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/view/SongLib.fxml"));
+        primaryStage.setTitle("Song Library");
+        primaryStage.setScene(new Scene(root, 800, 700));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/view/SongLibController.java
@@ -0,0 +1,170 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.collections.FXCollections;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+
+
+
+public class SongLibController {
+	
+    @FXML
+    private ListView<String> listView;
+	
+	@FXML
+	private TextField addsongName;
+	
+	@FXML
+	private TextField addartist;
+	
+	@FXML
+	private TextField addalbum;
+	
+	@FXML
+	private TextField addyear;
+	
+    @FXML
+    private TextField editsongName;
+
+    @FXML
+    private TextField editartist;
+
+    @FXML
+    private TextField editalbum;
+
+    @FXML
+    private TextField edityear;
+
+	
+    private ObservableList<String> obsList;
+    ArrayList<SongArtist> librarylist = new ArrayList<SongArtist>();
+    
+    ArrayList<String> stringlist = new ArrayList<String>();
+
+    
+    
+    
+    
+    
+    public void initialize(){        
+        
+        //read from songlist.txt and create array of SongArtist objects, also creates according array of String for listview
+        readSongArtist(librarylist,stringlist);
+        
+		// create an ObservableList 
+		// from an ArrayList  
+		obsList = FXCollections.observableArrayList(stringlist); 
+
+		listView.setItems(obsList); 
+		
+		// select the first item
+	    listView.getSelectionModel().select(0);
+	    
+    }
+
+    
+    
+    
+    
+    
+    //reads from file and populates librarylist!
+    public static void readSongArtist(ArrayList<SongArtist> librarylist, ArrayList<String> stringlist) {
+    	//read from text file
+        try {
+            Scanner s = new Scanner(new File("src/view/songlist.txt"));
+            
+            while (s.hasNextLine()) {
+
+            	String thisline = s.nextLine();
+            	String[] arrOfStr = thisline.split("\\*",4);
+            	String thissong = arrOfStr[0];
+            	String thisartist = arrOfStr[1];
+            	String thisalbum = arrOfStr[2];
+            	String thisyear = arrOfStr[3];
+            	stringlist.add(thissong+" - "+thisartist+" - "+thisalbum+" - "+thisyear);
+            	
+            	librarylist.add(new SongArtist(thissong,thisartist,thisalbum,thisyear));
+            	
+            }
+            s.close();
+            System.out.println(librarylist);
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex);
+        }
+    }
+    
+    //convert object list to string list
+    public ArrayList<String> objectToList(ArrayList<SongArtist> librarylist){
+    	
+    	int n = librarylist.size();
+    	ArrayList<String> listlist = new ArrayList<String>();
+    	
+    	for(int i=0;i<n;i++) {
+    		listlist.add(librarylist.get(i).toString());
+    	}
+    	
+    	//System.out.println(listlist);
+    	return listlist;
+    	
+    }    
+
+    //adding a SongArtist to obsList
+    public boolean add(SongArtist toAdd) {
+    	//not sure if check for null means anything addsongName.getText().equals(null) || addartist.getText().equals(null) || 
+    	if(addsongName.getText().equals("") || addartist.getText().equals("")) {
+    		return false;
+    	}
+    	for(int i=0;i<librarylist.size();i++) {
+    		if(librarylist.get(i).song.toLowerCase().equals(toAdd.song.toLowerCase())&&librarylist.get(i).artist.toLowerCase().equals(toAdd.artist.toLowerCase())) {
+        		return false;
+    		}
+    	}  	
+    	
+    	librarylist.add(toAdd);
+        Collections.sort(librarylist);   
+   		obsList = FXCollections.observableArrayList(objectToList(librarylist)); 
+  		listView.setItems(obsList); 
+        return true;
+    	
+    }
+    
+	// Button was clicked, do something…
+	//get songName, artist, album, year
+	//create new songArtist
+	//add to listlibrary and update obList
+	@FXML
+    void addbutton(ActionEvent event) {
+
+    	SongArtist toadd = new SongArtist(addsongName.getText(), addartist.getText(), addalbum.getText(), addyear.getText());
+    	System.out.println(toadd);
+    	if(!add(toadd)) {
+    		//send out an error message
+    		System.out.println("EITHER SONG ARTIST TEXT FIELD IS NULL OR SONG ARTIST ALREADY EXISTS");
+    	}
+    	
+    	//reset textboxes
+    	addsongName.clear();
+    	addartist.clear();
+    	addalbum.clear();
+    	addyear.clear();
+    }
+	
+	
+	@FXML
+	void deletebutton(ActionEvent event) {
+
+	}
+
+	
+}
